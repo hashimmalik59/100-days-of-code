3806fe1a22e91a9de1ab02d67dac0602c8bccd0b
hashimmalik59 <hm3630450@gmail.com>
2026-05-20 21:10:12 +0500

    Finding the per head cost with tip

--- 05-day/app.js
+++ 05-day/app.js
@@ -2,17 +2,13 @@ let numberOfPeople = +prompt("Enter number of people");
 let bill = +prompt("Enter bill");
 let tip = +prompt("Enter tip");
 
-if (
-  typeof numberOfPeople === "number" &&
-  typeof bill === "number" &&
-  typeof tip === "number"
-) {
+if (!isNaN(numberOfPeople) && !isNaN(bill) && !isNaN(tip)) {
   function tipCalculator(numberOfPeople, bill, tip) {
     let perHead = numberOfPeople / bill;
     let tipAmount = perHead * tip;
     return perHead + tipAmount;
   }
-} else console.log("Invalid data");
+} else console.log("Invalid Data!");
 
 console.log(
   `Per head cost with tip: ${tipCalculator(numberOfPeople, bill, tip)}`,
