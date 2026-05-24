a61fe761eb27f420e41e1ae73c77f75267cf7295
hashimmalik59 <hm3630450@gmail.com>
2026-05-24 15:27:09 +0500

    Day 8 of, 100 days of code for logic building, problem soving, debugging, thinking, and learning concepts

---
+++ 08-day/app.js
@@ -0,0 +1,18 @@
+let nums = [56, 45, 89, 67, 24];
+
+// // // function gradeCalculator(numsArr) {
+// // //   let passingGrades = [];
+// // //   for (let i = 0; i < numsArr.length; i++) {
+// // //     if (numsArr[i] >= 50) {
+// // //       passingGrades.push(`${i}: ${numsArr[i]}`);
+// // //     }
+// // //   }
+// // //   return passingGrades;
+// // // }
+
+// // // console.log(gradeCalculator(nums));
+
+// let passingGrades = nums
+//   .filter((marks) => marks >= 50)
+//   .map((marks, index) => `${index}: ${marks}`);
+// console.log(passingGrades);
